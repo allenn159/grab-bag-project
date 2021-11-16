df5b87be675c9012a55db27e1712c930bfcba572
Allen Abbott <allenabbottdev@gmail.com>
2021-11-15 20:56:48 -0800

    Grab Bag Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [devices, setDevices] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Grab Bag Project</h1>
+      <div
+        style={{ height: "300px", width: "900px", border: "5px solid red" }}
+      ></div>
+      <div></div>
     </div>
   );
 }
