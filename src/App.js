f90a965de9fa7d425f2d8cd69e3ef42f3215d814
Allen Abbott <allenabbottdev@gmail.com>
2021-11-15 21:29:18 -0800

    Set up axios to fetch data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,55 @@ import axios from "axios";
 
 function App() {
   const [devices, setDevices] = useState(null);
+  const [bagItems, setBagItems] = useState(null);
+  const [offset, setOffSet] = useState(7);
+  const [limit, setLimit] = useState(50);
+
+  async function getDevices() {
+    const { data } = await axios.get(
+      `https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=${offset}&limit=${limit}`
+    );
+    setDevices(data);
+  }
+
+  useEffect(() => {
+    getDevices();
+  }, []);
+
+  console.log(devices);
+
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <h1>Grab Bag Project</h1>
       <div
-        style={{ height: "300px", width: "900px", border: "5px solid red" }}
+        style={{
+          height: "300px",
+          width: "90%",
+          border: "5px solid red",
+          marginBottom: "30px",
+        }}
       ></div>
-      <div></div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {devices?.map((device) => (
+          <img
+            style={{ padding: "0 20px" }}
+            key={device.wikiid}
+            src={device.image.standard}
+          />
+        ))}
+      </div>
     </div>
   );
 }
